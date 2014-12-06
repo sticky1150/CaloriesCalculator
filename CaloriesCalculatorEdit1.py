ff6ca6bd98143766fe64f7ace2f14ee75b5755e5
Kritsada Salamteh <s5070003@kmitl.ac.th>
2014-12-06 15:03:59 +0700

    add calculate page

--- CaloriesCalculatorEdit1.py
+++ CaloriesCalculatorEdit1.py
@@ -4,18 +4,25 @@ from Tkinter import *
 import tkFont
 
 
-def ok():
+#-------calculate calories page------#
+def calculate():
+    rooto = Tk()
     food_dic = {"ข้าวผัดหมู":350, "แกงเขียวหวาน":400}
-    dessert_dic = {}
-    bevarage_dic = {}
+    dessert_dic = {"บัวลอย":450}
+    beverage_dic = {"เป็บซี่":150}
+    rooto.title("Calculator")
     food_name = food_var.get()
     dessert_name = dessert_var.get()
     beverage_name = beverage_var.get()
-    print food_name , food_dic[food_name.encode('utf8')]#show values of dic by thai language
-    rooto = Tk()
-    Label(rooto, text= food_dic[food_name.encode('utf8')] ).grid(row=0, sticky=W)#show label in callories
+    #find sum calories
+    calories = food_dic[food_name.encode('utf8')]+dessert_dic[dessert_name.encode('utf8')]+beverage_dic[beverage_name.encode('utf8')]
+    print "Calories" , calories
+    Label(rooto, text="จำนวนแคลอรี่ทั้งหมด", font=("Britannic Bold", 25)).grid(row=0, column=3)
+    Label(rooto, text=calories, font=("Britannic Bold", 25)).grid(row=1, column=3)#show callories in label
+    Label(rooto, text="การออกกำลังกายที่แนะนำ", font=("Britannic Bold", 18)).grid(row=3, column=3)
     rooto.mainloop()
 
+#------choose food page---------#
 def food():#select food page
     global food_var, dessert_var, beverage_var
     root.destroy()
@@ -29,7 +36,7 @@ def food():#select food page
     Label(rootf, text="ของหวาน",font=("Britannic Bold", 20)).grid(row=3, sticky=W)
     Label(rootf, text="เครื่องดื่ม",font=("Britannic Bold", 20)).grid(row=4, sticky=W)
     #------------Food Section------------------#
-    food = ('ข้าวผัดหมู','แกงเขียวหวาน','กบทอด','ข้าวไข่ดาว','ข้าวขาหมู','ข้าวไกย่าง')
+    food = ('ไม่เลือก','ข้าวผัดหมู','แกงเขียวหวาน','กบทอด','ข้าวไข่ดาว','ข้าวขาหมู','ข้าวไกย่าง')
     food_var = StringVar(rootf)
     food_var.set(food[0])
     food_lis = OptionMenu(rootf, food_var, *food)
@@ -38,7 +45,7 @@ def food():#select food page
     food_var.get()
     
     #----------Dessert Section---------------#
-    dessert = ('บัวลอย','ขนมโก๋','อิอิ','ครุคริ')
+    dessert = ('ไม่เลือก','บัวลอย','ขนมโก๋','อิอิ','ครุคริ')
     dessert_var = StringVar(rootf)
     dessert_var.set(dessert[0])
     dessert_lis = OptionMenu(rootf, dessert_var, *dessert)
@@ -46,19 +53,19 @@ def food():#select food page
     dessert_var.get()
 
     #------Beverage Section---------#
-    beverage = ('เป็บซี่','โคล่า','ชา','ครุคริ')
+    beverage = ('ไม่เลือก','เป็บซี่','โคล่า','ชา','ครุคริ')
     beverage_var = StringVar(rootf)
     beverage_var.set(beverage[0])
     beverage_lis = OptionMenu(rootf, beverage_var, *beverage)
     beverage_lis.grid(row=4, column=1)
     beverage_var.get()
     
-    button = Button(rootf, text="OK", command=ok)
+    button = Button(rootf, text="OK", command=calculate)
     button.grid(row=5, column=1)
     rootf.mainloop()
 
 
-
+#-------First Page------#
 root = Tk()
 root.geometry("400x350+200+200")
 font = tkFont.Font(size="25")
